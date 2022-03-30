b28675742bda0244fb92d53c8cd15865c3e66d74
Matheus Tostes <matheustostes@hotmail.com>
2022-03-30 08:25:24 -0300

    Core CRUD

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+const cursos = ["backend", "frontend", "dba", "bi"];
+
+server.get("/cursos/:index", (req, res) => {
+  const { index } = req.params;
+
+  return res.json(cursos[index]);
+});
+
+server.get("/cursos/", (req, res) => {
+  const { index } = req.params;
+
+  return res.json(cursos);
+});
+
+server.put("/cursos/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  cursos[index] = name;
+
+  return res.json(cursos);
+});
+
+server.delete("/cursos/:index", (req, res) => {
+  const { index } = req.params;
+
+  cursos.splice(index, 1);
+
+  return res.json(cursos);
+});
+
+server.listen(3010);
